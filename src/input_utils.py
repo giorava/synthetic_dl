4b5175e35b84d1b16ea98bae3cc47c221c3da880
Giorgio Ravanelli <giorava98@gmail.com>
2026-02-02 11:09:24 +0100

    unittests preprocessing utils

--- src/input_utils.py
+++ src/input_utils.py
@@ -39,7 +39,8 @@ def filter_overlaps(extended_peaks: pd.DataFrame) -> pd.DataFrame:
         overlaps_array.append(number_overlaps.item())
         
     overlapping_indx = np.where(np.array(overlaps_array) != 0)[0]
-    return extended_peaks.drop(index = overlapping_indx).reset_index()
+
+    return extended_peaks.drop(index = overlapping_indx).reset_index(drop=True)
 
 
 def extend_and_filter_overlaps(peaks: BedTool, chrom_sizes: pd.DataFrame, window_size: int) -> BedTool:
@@ -60,8 +61,8 @@ def extend_and_filter_overlaps(peaks: BedTool, chrom_sizes: pd.DataFrame, window
     Raises:
         AssertionError: If chrom_sizes doesn't have exactly 2 columns (chrom id and lenght)
     """
-    
-    assert chrom_sizes.shape[1] != 2, "Check that the fist column of chrom sizes are chromosome names"
+    print(chrom_sizes.shape[1])
+    assert chrom_sizes.shape[1] == 2, "Check that the fist column of chrom sizes are chromosome names"
     chrom_sizes.index = chrom_sizes.iloc[:,0]
 
     logging.info(f"Extending {peaks.__len__()} peaks")
@@ -69,7 +70,7 @@ def extend_and_filter_overlaps(peaks: BedTool, chrom_sizes: pd.DataFrame, window
     for peak in tqdm.tqdm(peaks):
         chrom, start, end = peak.chrom, peak.start, peak.end
         chrom_size = chrom_sizes.loc[chrom][1].item()
-        mid_peak = (end-start)//2
+        mid_peak = start + (end-start)//2
         lower_bound = mid_peak-window_size//2
         upper_bound = mid_peak+window_size//2
 
@@ -84,7 +85,7 @@ def extend_and_filter_overlaps(peaks: BedTool, chrom_sizes: pd.DataFrame, window
     logging.info(f"Number of submitted peaks: {peaks.__len__()}")
     logging.info(f"Number of non overlapping peaks: {filtered.shape[0]}")
 
-    return pybedtools.BedTool.from_dataframe(filtered, from_list=True)
+    return pybedtools.BedTool.from_dataframe(filtered)
 
 
 def check_for_Ns(peaks: BedTool, genome_fasta_path: str) -> pd.DataFrame: 
@@ -119,143 +120,3 @@ def check_for_Ns(peaks: BedTool, genome_fasta_path: str) -> pd.DataFrame:
     return pd.DataFrame(surviving_peaks)
 
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import torch, tqdm
-# from torch import nn
-# import pandas as pd
-# import numpy as np
-# import pybedtools, tqdm, pyBigWig
-# from torch.utils.data import Dataset
-# from torch.utils.data import DataLoader 
-
-# import datasets_manager, models
-# import matplotlib.pyplot as plt
-# from torchvision import transforms
-# import scipy, argparse, warnings
-
-# class prepareInput(): 
-
-#     def __init__(self, datadir: str, regions: str, chromsizes: str, genomefasta: str, input_length: int,
-#                         bigwigs: list[str], test_chrom: list[str], train_chrom: list[str], val_chrom: list[str]):
-
-#         self.datadir = datadir
-#         self.regions_file = regions
-#         self.chromsizes_file = chromsizes
-#         self.genomefasta_file = genomefasta
-#         self.bigwigs_files = bigwigs
-#         self.test_chrom = test_chrom
-#         self.train_chrom = train_chrom
-#         self.val_chrom = val_chrom
-#         self.input_length = input_length
-
-#         self.regions = pybedtools.BedTool(self.regions_file)
-#         self.chromsizes = pd.read_csv(self.chromsizes_file, sep = "\t", header = None)
-
-#         self.n_heads = len(bigwigs)
-#         self.n_tasks = len(bigwigs[0]) 
-
-#         self.bws = [[pyBigWig.open(j) for j in i] for i in bigwigs]
-
-#     def get_bin_centered(self, coords: list, ext: int):
-
-#         center = (coords[1]+coords[2])//2
-#         new_low, new_high = center-ext, center+ext
-#         size = self.chromsizes[self.chromsizes.iloc[:, 0] == coords[0]][1].to_numpy()[0]
-
-#         if (new_low>1)&(new_high<size):
-#             return (coords[0], new_low, new_high)
-#         else: 
-#             return None
-        
-
-#     def one_hot_and_check(self, coords): 
-#         chrom, start, end = coords
-#         sequence = pybedtools.BedTool.seq((chrom, start, end), self.genomefasta_file).upper()
-#         count_Ns = sequence.count("N")
-    
-#         if count_Ns == 0: 
-#             mapping = dict(zip("ACGT", range(4)))
-#             hot_enc = np.zeros((len(sequence), 4))
-#             hot_enc[np.arange(len(sequence)), [mapping[i] for i in sequence]] = 1
-#             return hot_enc
-#         else: 
-#             return None
-        
-        
-#     def bigwigs_binned(self, coords): 
-
-#         for head_idx in range(self.n_heads):
-#             task_signal = []
-#             for task_idx in range(self.n_tasks): 
-#                 bw_values = self.bws[head_idx][task_idx].values(coords[0], start = coords[1], end = coords[2])
-#                 task_signal.append(np.nan_to_num(bw_values))
-
-        
-#     def get_signal_binned(self):
-
-#         if self.regions.shape[1]>3:
-#                 warnings.warn("BED file with more than 3 columns, only chrom, strat and end will be considered")
-
-#         for i, region in tqdm.tqdm(self.regions):
-#             cordinates = region.chrom, region.start, region.end
-#             centered_bin = self.get_bin_centered(cordinates, ext = self.input_length//2) 
-#             if centered_bin != None: 
-#                 hot_encoding = self.one_hot_and_check(cordinates)
-#                 if hot_encoding != None: 
-
-#                     ## convert hot_encoding_to_tensor
-#                     ## get tensor data
-
-
-
-
-
-
-        
-
-#         # for region in regions
-#         #       get the coordinates
-#         #       center the bin
-#         #.      if centered bin is not None:
-#         #           one hot encoding of the sequence
-#         #           if one hot encoded is not None:
-#         #               signal_head = []
-#         #               for each task in bigwigs.shape[0]:
-#         #                      read the 
-    
-        
-
--- tests/preprocessing_tests.py
+++ tests/preprocessing_tests.py
@@ -1,7 +1,8 @@
 import unittest
-import pybedtools
+import pybedtools, sys
 import pandas as pd
-import src.input_utils as data_utils
+sys.path.insert(0, "../src")
+import input_utils as data_utils
 
 class test_input_utils(unittest.TestCase):
 
@@ -12,14 +13,99 @@ class test_input_utils(unittest.TestCase):
         self.chrom_sizes = pd.read_csv(chrom_sizes, sep = "\t", header = None)
         self.extend = ext
 
-    def test_find_overlaps(): 
-        pass
-    
-    def test_extend_and_filter_overlaps():
-        pass 
+    def assert_equal_bed(self, bed1, bed2): 
+
+        bed1_df = bed1.to_dataframe()
+        bed2_df = bed2.to_dataframe()
+        pd.testing.assert_frame_equal(bed1_df, bed2_df)
+
+    def test_find_overlaps(self): 
+        
+        w1 = pd.DataFrame([
+            ("chr1",   1, 100),
+            ("chr1",  30, 130),
+            ("chr1", 200, 250),
+            ("chr2",   1, 100)
+        ])
+        w1_post = pd.DataFrame([
+            ("chr1", 200, 250),
+            ("chr2",   1, 100)
+        ])
+        filtered_w1 = data_utils.filter_overlaps(w1)
+        pd.testing.assert_frame_equal(filtered_w1, w1_post)
+
+        w2 = pd.DataFrame([
+            ("chr1",   1, 100),
+            ("chr1",   1, 100),
+            ("chr1",  80, 150),
+            ("chr2",   1, 100), 
+            ("chr2",   80, 140),
+            ("chr3",   80, 140)
+        ])
+        w2_post = pd.DataFrame([
+            ("chr3",   80, 140)
+        ])        
+        filtered_w2 = data_utils.filter_overlaps(w2)
+        pd.testing.assert_frame_equal(filtered_w2, w2_post)
+
+    def test_extend_and_filter_overlaps(self):
+
+        chrom_sizes = pd.DataFrame([
+            ("chr1",   10000),
+            ("chr2",  10000),
+        ])
+
+        # checking that we are filtering out stuff outside ranges lower
+        w1 = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr1",   1, 10),
+            ("chr2",  150, 600),
+        ]))
+        w1_post = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr2",  (150+225)-25, (150+225)+25),
+        ]))
+        filtered_w1 = data_utils.extend_and_filter_overlaps(
+            peaks=w1, chrom_sizes=chrom_sizes, window_size=50
+        )
+        self.assert_equal_bed(filtered_w1, w1_post)
+
+
+        # checking ranges that become overlapping after extension
+        w2 = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr1", 400, 500),
+            ("chr1", 598, 602),
+            ("chr2",  150, 600),
+        ]))
+        w2_post = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr2",  (150+225)-200, (150+225)+200),
+        ]))
+        filtered_w2 = data_utils.extend_and_filter_overlaps(
+            peaks=w2, chrom_sizes=chrom_sizes, window_size=400
+        )
+        self.assert_equal_bed(filtered_w2, w2_post)
+
+
+        # checking that we are filtering out stuff outside ranges upper
+        w3 = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr1",   10000-2, 10000),
+            ("chr2",   150, 600),
+        ]))
+        w3_post = pybedtools.BedTool.from_dataframe(pd.DataFrame([
+            ("chr2",  (150+225)-25, (150+225)+25),
+        ]))
+
+        filtered_w3 = data_utils.extend_and_filter_overlaps(
+            peaks=w3, chrom_sizes=chrom_sizes, window_size=50
+        )
+        self.assert_equal_bed(filtered_w3, w3_post)
+
 
     def check_for_Ns():
         pass
 
 if __name__=="__main__": 
-    pass
+    
+    obj = test_input_utils(peaks_files = "../demos/example_datasets/peaks.bed",
+                        chrom_sizes = "/homes/users/gravanelli/reference_genomes/mm10.chrom.sizes",
+                        ext = 1000)
+    obj.test_find_overlaps()
+    obj.test_extend_and_filter_overlaps()
