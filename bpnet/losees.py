2eaa759b9176eea320421232a693cce5f121d54e
Giorgio Ravanelli <giorava98@gmail.com>
2026-02-04 17:45:50 +0100

    added losses

--- bpnet/losees.py
+++ bpnet/losees.py
@@ -1,16 +1,13 @@
 import torch 
 from torch import nn
-import pandas as pd
-import numpy as np
-import pybedtools, tqdm, pyBigWig
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-
+from torch.distributions import Multinomial
 
 class BPNetLosses(nn.Module):
-    def __init__(self):   
+    
+    def __init__(self, num_tasks: int):   
         super().__init__()
 
+        self.num_tasks = num_tasks
         self.profile_loss = 0
         self.count_loss = 0
 
@@ -21,32 +18,50 @@ class BPNetLosses(nn.Module):
         return self.count_loss
 
     def MSE(self, target_counts, pred_counts):
+        
+        tot_target_counts = torch.sum(target_counts, 1)
+        tot_pred_counts = torch.sum(pred_counts, 1)
+        mean_squared_error = torch.mean((tot_target_counts - tot_pred_counts)**2, 0)
+        return mean_squared_error
 
-        # compute simple MSE, target counts tensor is BatchSize x 1 and so is the pred_counts vector
+    def multinomial_nll(self, target_profile, pred_logits): 
 
-        pass
+        batch_size = pred_logits.shape[0]
 
+        flatTask = torch.flatten(target_profile, 1)
+        flatCounts = torch.sum(flatTask, 1)
+        
+        flatLogits = torch.flatten(pred_logits, 1)
+        
+        batch_multinomial_nll = 0
+        for i in range(batch_size): 
+            
+            dist = Multinomial(
+                total_count=int(flatCounts[i].item()),
+                logits=flatLogits[i]
+            )
+            mult_nll = -dist.log_prob(flatTask[i])
+            batch_multinomial_nll += mult_nll
+            
+        return batch_multinomial_nll/batch_size
+        
+        
+    def forward(self, pred_counts, target_counts, pred_prof, target_prof, count_weights):
+
+        counts_raw_loss = self.MSE(
+            target_counts = target_counts, 
+            pred_counts = pred_counts
+        )
+
+        prof_raw_loss = self.multinomial_nll(
+            target_profile = target_prof,
+            pred_logits = pred_prof
+        )
+
+        self.profile_loss = prof_raw_loss
+        self.count_loss = counts_raw_loss
+        
+        combined = prof_raw_loss+count_weights*counts_raw_loss
+
+        return combined
     
-    def multinomial_nll(self, task_target_counts, logits_task): 
-
-        # task_target_counts should have shape (batch_size, output_length)
-        # the logits_tracs should have shape (batch_size, output_length)
-
-        pass 
-        
-    def forward(self, input, target, counts_weights):
-        
-        # counts_loss = []
-        # profile_los = []
-        # for i in n_heads: 
-        #       
-        #       compute multinomial_nll on the profiles
-        #       compute weighted MSE on the counts
-
-        ## combined = torch.sum(counts_weight*counts_loss) + torch.sum(profile_los)
-        ## return combined 
-
-        # self.profile_loss = torch.sum(profile_los)
-        # self.count_loss = torch.sum(counts_loss)
-        pass 
-
